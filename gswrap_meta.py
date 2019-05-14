2dbf4b17c15ed6d81c4512c9b3d8c774a71e346c
Marko Ristin <marko.ristin@gmail.com>
2019-05-14 11:44:23 +0200

    bumped to 1.0.4 (#28)

--- gswrap_meta.py
+++ gswrap_meta.py
@@ -3,7 +3,7 @@
 __title__ = 'gs-wrap'
 __description__ = 'gs-wrap wraps Google Cloud Storage API for multi-threaded data manipulations.'
 __url__ = 'https://github.com/Parquery/gs-wrap'
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 __author__ = 'Selim Naji, Adam Radomski and Marko Ristin'
 __author_email__ = 'selim.naji@parquery.com, adam.radomski@parquery.com, marko.ristin@gmail.com'
 __license__ = 'MIT'
